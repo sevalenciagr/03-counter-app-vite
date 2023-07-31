a8d526dd6d14660151badab14355d93d9713cdfb
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-07-31 16:00:48 -0500

    Haciendo primeros test

---
+++ src/base-pruebas/05-funciones.js
@@ -0,0 +1,16 @@
+export const getUser = () => ({
+        uid: 'ABC123',
+        username: 'El_Papi1502'
+});
+
+
+// Tarea
+export const getUsuarioActivo = ( name ) =>({
+    uid: 'ABC567',
+    username: name
+})
+
+
+
+
+
---
+++ tests/base-pruebas/02-template-string.test.js
@@ -0,0 +1,13 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
+
+describe('Pruebas en 02-template-string', () => {
+
+    test('getSaludo debe de retornar "Hola Sebastian "', () =>{
+
+        const name = 'Sebastian';
+        const message = getSaludo( name );
+
+        expect( message ).toBe(`Hola ${ name }`);
+
+    });
+});
